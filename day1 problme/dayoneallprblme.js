5881fac81eb535558141b7915ad7480e4afb310d
hakimcolor <hakimcolor777@gmail.com>
2026-06-16 20:19:41 +0600

    celsius to fahrenheit

--- day1 problme/dayoneallprblme.js
+++ day1 problme/dayoneallprblme.js
@@ -40,3 +40,15 @@ function largest(a, b, c) {
   }
 }
 console.log(largest(3, 7, 5));
+
+// Problem 4: Celsius to Fahrenheit  [Easy]
+// Description: Write a function toFahrenheit(celsius) that converts a Celsius temperature to Fahrenheit.
+// Example:
+// Input: 0   → Output: 32Input: 100 → Output: 212
+// Hint: Formula: (C × 9/5) + 32
+function toFahrenheit(celsius) {
+  let result = (celsius * 9) / 5 + 32;
+  return result;
+}
+
+console.log(toFahrenheit(100));
