c085553371560219cb902e032a40b6543ca47128
hakimcolor <hakimcolor777@gmail.com>
2026-06-17 20:51:41 +0600

    fibonacci sequence

--- day5 problem/day5problem.js
+++ day5 problem/day5problem.js
@@ -17,3 +17,16 @@ console.log(factorial(5));
 // Example:
 // Input: 6  → Output: 8 (0,1,1,2,3,5,8...)
 // Hint: Try both iterative and recursive approaches.
+function fibonacci(n) {
+  if (n <= 1) {
+    return n;
+  }
+  let fib = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[n];
+}
+
+console.log(fibonacci(6));
