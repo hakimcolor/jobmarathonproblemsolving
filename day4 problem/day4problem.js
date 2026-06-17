e8200d7078d51ffd87ba1e83847feb0d39f42a33
hakimcolor <hakimcolor777@gmail.com>
2026-06-17 20:35:43 +0600

    invert an object

--- day4 problem/day4problem.js
+++ day4 problem/day4problem.js
@@ -45,4 +45,22 @@ fizzBuzz(15);
 // Description: Write a function invertObject(obj) that swaps the keys and values of an object.
 // Example:
 // Input: {a: 1, b: 2}  → Output: {1: 'a', 2: 'b'}
-// Hint: Use Object.entries() and reduce().
+// Hint: Use Object.entries() and reduce().র
+function invertObject (obj) {
+  let inverted = {};
+
+  for(let key in obj){
+    let value = obj[key];
+    inverted[value] = key;
+  }
+  return inverted;
+}
+
+let result = invertObject({ a: 1, b: 2 });
+
+let outputParts = [];
+for (let key in result) {
+  outputParts.push(`${key}: '${result[key]}'`); 
+}
+
+console.log(`{ ${outputParts.join(', ')} }`);
