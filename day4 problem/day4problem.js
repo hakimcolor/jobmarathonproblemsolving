53263300155327abb8b18458c404cee3ca485705
hakimcolor <hakimcolor777@gmail.com>
2026-06-17 20:31:19 +0600

    marge to object console

--- day4 problem/day4problem.js
+++ day4 problem/day4problem.js
@@ -9,7 +9,6 @@ function countProperties(obj) {
 }
 console.log(countProperties({ a: 1, b: 2, c: 3 }));
 
-
 // Problem 17: Merge Two Objects  [Easy]
 // Description: Write a function mergeObjects(obj1, obj2) that merges two objects into one. If keys conflict, the second object's values win.
 // Example:
@@ -18,4 +17,5 @@ console.log(countProperties({ a: 1, b: 2, c: 3 }));
 function mergeObjects(obj1, obj2) {
   let mergeObj = { ...obj1, ...obj2 };
   return mergeObj;
-} 
+}
+console.log(mergeObjects({ a: 1 }, { b: 2 }));
