2f79bfc2542e3626af6effff2d4ae5722461fe9e
hakimcolor <hakimcolor777@gmail.com>
2026-06-17 20:29:55 +0600

    marge two objects

--- day4 problem/day4problem.js
+++ day4 problem/day4problem.js
@@ -7,5 +7,11 @@ function countProperties(obj) {
   let arrObj = Object.keys(obj).length;
   return arrObj;
 }
-
 console.log(countProperties({ a: 1, b: 2, c: 3 }));
+
+
+// Problem 17: Merge Two Objects  [Easy]
+// Description: Write a function mergeObjects(obj1, obj2) that merges two objects into one. If keys conflict, the second object's values win.
+// Example:
+// Input: {a:1}, {b:2}  → Output: {a:1, b:2}
+// Hint: Use the spread operator or Object.assign().
