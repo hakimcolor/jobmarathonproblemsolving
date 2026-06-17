443419c6b7c15f5a99cf805b9326da3e5e166015
hakimcolor <hakimcolor777@gmail.com>
2026-06-17 20:26:26 +0600

    chunk an arry fix ans

--- day3 problem/day3problme.js
+++ day3 problem/day3problme.js
@@ -14,18 +14,17 @@ function sumArray(arr) {
 
 console.log(sumArray([1, 2, 3, 4, 5]));
 
-
 // Problem 12: Find Maximum Value in Array  [Easy]
 // Description: Write a function findMax(arr) that returns the largest number in an array without using Math.max().
 // Example:
 // Input: [3, 1, 7, 2, 9]  → Output: 9
 // Hint: Loop through and track the largest value found.
 
-function findMax (arr) {
+function findMax(arr) {
   let max = [0];
-  
-  for(let i = 0; i < arr.length; i++) {
-    if(max < arr[i]) {
+
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
       max = arr[i];
     }
   }
@@ -38,13 +37,13 @@ console.log(findMax([1, 2, 3, 4, 5]));
 // Example:
 // Input: [1, 2, 2, 3, 3, 4]  → Output: [1, 2, 3, 4]
 // Hint: Use Set or filter() with indexOf().
-function removeDuplicates (arr) {
+function removeDuplicates(arr) {
   let uniqueArray = arr.filter((element, index) => {
     return arr.indexOf(element) === index;
   });
   return uniqueArray;
 }
-console.log(removeDuplicates([1, 2, 3, 4, 5, 5, 1, 6, 7, 7, 2,]));
+console.log(removeDuplicates([1, 2, 3, 4, 5, 5, 1, 6, 7, 7, 2]));
 
 // Problem 14: Flatten a Nested Array  [Medium]
 // Description: Write a function flattenArray(arr) that flattens one level of a nested array.
@@ -52,9 +51,8 @@ console.log(removeDuplicates([1, 2, 3, 4, 5, 5, 1, 6, 7, 7, 2,]));
 // Input: [1, [2, 3], [4, 5]]  → Output: [1, 2, 3, 4, 5]
 // Hint: Use flat() or reduce() with concat().
 
-
-function flattenArray (arr) {
-return arr.flat();
+function flattenArray(arr) {
+  return arr.flat();
 }
 
 console.log(flattenArray([1, [2, 3], [4, 5]]));
@@ -64,3 +62,13 @@ console.log(flattenArray([1, [2, 3], [4, 5]]));
 // Example:
 // Input: [1,2,3,4,5], 2  → Output: [[1,2],[3,4],[5]]
 // Hint: Use a while loop with slice().
+function chunkArray(arr, size) {
+  let chunked = [];
+  let index = 0;
+  while (index < arr.length) {
+    chunked.push(arr.slice(index, index + size));
+    index = index + size;
+  }
+  return chunked;
+}
+console.log(chunkArray([1, 2, 3, 4, 5], 2));
