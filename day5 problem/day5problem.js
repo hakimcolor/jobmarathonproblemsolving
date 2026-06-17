dd69200c528baae7eba388b56bd2b6791c97cf4b
hakimcolor <hakimcolor777@gmail.com>
2026-06-17 20:50:59 +0600

    fibonacci saquence

--- day5 problem/day5problem.js
+++ day5 problem/day5problem.js
@@ -11,3 +11,9 @@ function factorial(n) {
 }
 
 console.log(factorial(5));
+
+// Problem 22: Fibonacci Sequence  [Easy]
+// Description: Write a function fibonacci(n) that returns the nth number in the Fibonacci sequence.
+// Example:
+// Input: 6  → Output: 8 (0,1,1,2,3,5,8...)
+// Hint: Try both iterative and recursive approaches.
