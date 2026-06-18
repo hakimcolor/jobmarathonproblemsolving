e479f0704b656525c2ce3f3d3ad638b82acc7d8b
hakimcolor <hakimcolor777@gmail.com>
2026-06-18 23:02:18 +0600

    cury a function

--- day5 problem/day5problem.js
+++ day5 problem/day5problem.js
@@ -67,3 +67,12 @@ const c = makeCounter();
 // Example:
 // const add = curry((a,b) => a+b);add(2)(3); // 5
 // Hint: Return a function from inside a function.
+function curry(fn) {
+  return function (a) {
+    return function (b) {};
+  };
+}
+
+const curriedAdd = curry((a, b) => a + b);
+
+console.log(curriedAdd(2)(3)); 
