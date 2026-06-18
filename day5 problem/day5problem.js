4595ce340894bcd43d061968236e2fd5fbd538c7
hakimcolor <hakimcolor777@gmail.com>
2026-06-18 23:00:14 +0600

    problem 23

--- day5 problem/day5problem.js
+++ day5 problem/day5problem.js
@@ -30,3 +30,9 @@ function fibonacci(n) {
 }
 
 console.log(fibonacci(6));
+
+// Problem 23: Create a Counter with Closure  [Medium]
+// Description: Write a function makeCounter() that returns an object with increment, decrement, and getCount methods using closure.
+// Example:
+// const c = makeCounter();c.increment(); c.increment();c.getCount(); // 2
+// Hint: Use a variable inside the outer function that inner functions can access.
