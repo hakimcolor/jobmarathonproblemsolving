ac830f9e4425522865c4726433de972337c874c3
hakimcolor <hakimcolor777@gmail.com>
2026-06-20 10:05:22 +0600

    implement arry prototype mp from scratch

--- day7 problem/day7problem.js
+++ day7 problem/day7problem.js
@@ -139,3 +139,30 @@ emitter.emit('greet', 'Sara');
 emitter.off('greet', greetSara);
 
 emitter.emit('greet', 'Sara');
+
+
+// Problem 35: Implement Array.prototype.map from Scratch  [Medium]
+// Description: Write a function myMap(arr, callback) that replicates the behavior of Array.prototype.map without using the built-in map().
+// Example:
+// myMap([1,2,3], x => x * 2);// Output: [2, 4, 6]
+// Hint: Loop through the array, apply the callback to each element, push results to a new array.
+
+
+// Problem 35: Implement Array.prototype.map from Scratch  [Medium]
+
+function myMap (arr, callback) {
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const newValue = callback(arr[i], i, arr); 
+    result.push(newValue); 
+  }
+
+  return result; 
+}
+
+const nums = [1, 2, 3];
+const doubled = myMap(nums, x => x * 2);
+
+console.log("Original Array:", nums);  
+console.log("Transformed Array:", doubled); 
