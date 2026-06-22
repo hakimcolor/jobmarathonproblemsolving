65d7268e3645279e0020a159ec41d7e6408de170
hakimcolor <hakimcolor777@gmail.com>
2026-06-22 18:54:15 +0600

    tow sum ans

--- day 9 problem/day9problem.js
+++ day 9 problem/day9problem.js
@@ -3,3 +3,24 @@
 // Example:
 // Input: [2, 7, 11, 15], target = 9Output: [0, 1]  (because 2 + 7 = 9)
 // Hint: Use a hash map to store visited values and their indices.
+function twoSum(arr, target) {
+  const map = {};
+
+  for (let i = 0; i < arr.length; i++) {
+    const complement = target - arr[i];
+
+    if (map.hasOwnProperty(complement)) {
+      return [map[complement], i];
+    }
+
+    map[arr[i]] = i;
+  }
+
+  return [];
+}
+
+
+const nums = [2, 7, 11, 15];
+const target = 9;
+
+console.log(twoSum(nums, target)); 
