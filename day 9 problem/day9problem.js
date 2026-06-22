f55b97db8646f5ee5d2b9024ef12743fa0de7746
hakimcolor <hakimcolor777@gmail.com>
2026-06-22 18:53:08 +0600

    two sum for nine day first commit

---
+++ day 9 problem/day9problem.js
@@ -0,0 +1,5 @@
+// Problem 41: Two Sum  [Easy]
+// Description: Given an array of numbers and a target, write a function twoSum(arr, target) that returns the indices of the two numbers that add up to the target.
+// Example:
+// Input: [2, 7, 11, 15], target = 9Output: [0, 1]  (because 2 + 7 = 9)
+// Hint: Use a hash map to store visited values and their indices.
