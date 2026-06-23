f1525f030c06e9d43358230838b78ae094c3b13c
hakimcolor <hakimcolor777@gmail.com>
2026-06-23 21:17:00 +0600

    valid parentheses

--- day 9 problem/day9problem.js
+++ day 9 problem/day9problem.js
@@ -50,4 +50,33 @@ console.log(isAnagram('hello', 'world'));
 // Hint: Use a stack (array). Push opening brackets, pop and compare for closing ones.
 
 
-// Problem 44: Valid Parentheses  [Medium]
+
+
+function isValidParentheses(str) {
+  const stack = [];
+
+  const bracketMap = {
+    ')': '(',
+    '}': '{',
+    ']': '[',
+  };
+
+  for (const char of str) {
+    if (bracketMap[char]) {
+      const topElement = stack.pop();
+
+      if (topElement !== bracketMap[char]) {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+}
+
+console.log(isValidParentheses('()[]{}'));
+console.log(isValidParentheses('([)]'));
+console.log(isValidParentheses('{[]}'));
+console.log(isValidParentheses('('));  
