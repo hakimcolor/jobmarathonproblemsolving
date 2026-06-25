6233f1126893b2fb8dc03150f26e355bc7b0fab7
hakimcolor <hakimcolor777@gmail.com>
2026-06-25 17:21:45 +0600

    binary serch ans

--- day 9 problem/day9problem.js
+++ day 9 problem/day9problem.js
@@ -85,3 +85,29 @@ console.log(isValidParentheses('('));
 //     Example:
 // Input: [1, 3, 5, 7, 9], target = 7  → Output: 3
 // Hint: Use left and right pointers; check the middle element each iteration.
+// Problem 45: Binary Search
+
+function binarySearch(arr, target) {
+  let left = 0;
+  let right = arr.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (arr[mid] === target) {
+      return mid;
+    }
+
+    if (arr[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+// Example
+console.log(binarySearch([1, 3, 5, 7, 9], 7)); // Output: 3
+console.log(binarySearch([1, 3, 5, 7, 9], 4)); 
