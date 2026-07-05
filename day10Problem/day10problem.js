746f90bc51055f32951684403647028ed347d8b7
hakimcolor <hakimcolor777@gmail.com>
2026-07-05 21:43:37 +0600

    j problme 50 pascals triangle row

--- day10Problem/day10problem.js
+++ day10Problem/day10problem.js
@@ -80,7 +80,7 @@ function romanToInt(s) {
 }
 console.log(romanToInt('III'));  
 
- Problem 50: Pascal's Triangle Row  [Medium]
+ //Problem 50: Pascal's Triangle Row  [Medium]
 // Description: Write a function pascalRow(n) that returns the nth row of Pascal's Triangle as an array.
 // Example:
 // Input: 0  → Output: [1]Input: 3  → Output: [1,3,3,1]Input: 4  → Output: [1,4,6,4,1]
