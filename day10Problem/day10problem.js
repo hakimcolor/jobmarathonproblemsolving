712981d3427789fa8d1f49d802d7230f80faaa4c
hakimcolor <hakimcolor777@gmail.com>
2026-07-06 22:25:39 +0600

    let i o i str lenth iplus

--- day10Problem/day10problem.js
+++ day10Problem/day10problem.js
@@ -114,6 +114,10 @@ console.log(pascalRow(3));
 function countVowels(str) {
   let count = 0;
     const vowels = 'aeiouAEIOU';
-
+ for (let i = 0; i < str.length; i++) {
+   if (vowels.includes(str[i])) {
+     count++;
+   }
+ }
 
 }
