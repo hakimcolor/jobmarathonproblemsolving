289fcb701e66c8927f9c4ff028b645f32477299f
hakimcolor <hakimcolor777@gmail.com>
2026-07-15 00:23:07 +0600

    solve

--- day10Problem/day10problem.js
+++ day10Problem/day10problem.js
@@ -277,4 +277,15 @@ function lengthOfLongestSubstring(s) {
   let map = new Map();
   let left = 0;
   let maxLength = 0;
+
+    for (let right = 0; right < s.length; right++) {
+    let char = s[right];
+ if (map.has(char) && map.get(char) >= left) {
+      left = map.get(char) + 1;
+    }
+    map.set(char, right);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+  return maxLength;
 }
+console.log(lengthOfLongestSubstring('abcabcbb')); 
